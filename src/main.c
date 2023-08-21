1c9b06c66596d019e1ef8c7704422e4cf3a1c70b
Grolsch <grolsch-sec@protonmail.com>
2023-08-21 20:03:13 +0200

    just testing the struct

--- src/main.c
+++ src/main.c
@@ -6,16 +6,20 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:04:55 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/08/21 19:55:30 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/08/21 20:02:52 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
 	t_game		game_info;
 	t_texture	path;
+
+	(void)path;
+	(void)argv;
 	if (argc != 2)
 		ft_putstr_fd("Error\nNot enough arguments", 1);
 	/*parse_mapinfo(&gameinfo, &path);*/
