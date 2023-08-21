2e7fa0359b812443db6d446f0e9bfb12f8061d81
Grolsch <grolsch-sec@protonmail.com>
2023-08-21 18:54:32 +0200

    adding mlx submodule

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,12 +6,13 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:05:17 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/08/21 18:28:14 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/08/21 18:52:12 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 # include <math.h>
-
+# include "../src/libft/libft.h"
+#
 #endif
