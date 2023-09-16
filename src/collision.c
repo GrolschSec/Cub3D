2961573ed18cf9a29420fa43f1fe120ef914942e
Younes Aassila <yaassila@student.42.fr>
2023-09-16 11:55:35 +0200

    Fix norm

--- src/collision.c
+++ src/collision.c
@@ -14,14 +14,14 @@
 
 /**
  * @brief Checks if a given position on the game map is valid.
- * 
+ *
  * @param game Holds game's current state and configurations.
  * @param new_x The x-coordinate of the position to check.
  * @param new_y The y-coordinate of the position to check.
  * @return 1 if the position is valid; otherwise, 0.
- * 
- * The function ensures the player doesn't collide with the edges 
- * or any obstacles ('1' on the map grid). Positions overlapping 
+ *
+ * The function ensures the player doesn't collide with the edges
+ * or any obstacles ('1' on the map grid). Positions overlapping
  * with obstacles or out-of-bounds are deemed invalid.
  */
 int	is_valid_position(t_game *game, double new_x, double new_y)
@@ -31,13 +31,12 @@ int	is_valid_position(t_game *game, double new_x, double new_y)
 		return (0);
 	if (game->map[(int)(new_y
 			- PLAYER_RADIUS)][(int)(new_x - PLAYER_RADIUS)] == '1' ||
-		game->map[(int)(new_y 
+		game->map[(int)(new_y
 			+ PLAYER_RADIUS)][(int)(new_x - PLAYER_RADIUS)] == '1' ||
 		game->map[(int)(new_y
 			- PLAYER_RADIUS)][(int)(new_x + PLAYER_RADIUS)] == '1' ||
 		game->map[(int)(new_y
 			+ PLAYER_RADIUS)][(int)(new_x + PLAYER_RADIUS)] == '1')
 		return (0);
-
 	return (1);
 }
